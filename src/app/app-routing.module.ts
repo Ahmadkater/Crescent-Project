1928412a31e5da014192f770d0751c52f6d45d82
Ahmadkater <ahmad.said3439@yahoo.com>
2019-09-23 21:42:04 +0200

    added consultation path to nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { ConsultationsComponent } from './consultations/consultations.component';
 
 
 const routes: Routes = [
   { path: 'home' , component:HomeComponent },
-  { path: 'aboutus' , component:AboutusComponent}
+  { path: 'aboutus' , component:AboutusComponent},
+  {path:'consultation' , component: ConsultationsComponent}
+  
 ];
 
 @NgModule({
