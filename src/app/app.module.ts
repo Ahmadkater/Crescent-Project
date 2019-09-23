87114d6be2b01fe5e44863fbdec85afa2477364b
Ahmadkater <ahmad.said3439@yahoo.com>
2019-09-23 03:45:13 +0200

    Add tabs->finished home tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ConsultationsComponent } from './consultations/consultations.component';
+import { ProductsComponent } from './products/products.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { PartnersComponent } from './partners/partners.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ConsultationsComponent,
+    ProductsComponent,
+    ProjectsComponent,
+    ResourcesComponent,
+    ContactsComponent,
+    PartnersComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
